8efa82c8399f7bdfe2f40a6e029ff2ea420a1356
Drew Atkison <dtatkison@gmail.com>
2023-12-07 17:48:18 -0600

    env updated - staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    BASE_URL: 'http://192.168.12.72:9000/api/',
+    BASE_URL: 'http://192.168.12.71:9000/api/',
     ASSET_URL: '/assets/'
   };
   
