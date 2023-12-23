def9738dabf591d4e3e50b1a3c2f259c71954d47
Drew Atkison <dtatkison@gmail.com>
2023-12-23 15:44:17 -0600

    add shake bug report to android

--- android/app/src/main/java/com/familybudget/app/MainActivity.java
+++ android/app/src/main/java/com/familybudget/app/MainActivity.java
@@ -1,5 +1,19 @@
 package com.familybudget.app;
+import com.shakebugs.shake.Shake;
+
+
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+    super.onCreate(savedInstanceState, persistentState);
+    Shake.start(this, "6kngWVlEFJFYTfhaeIGWmQxxRSUBNLgAFnfu6kY5", "iXpHIMjUxUBTK5uvBL540N8WEFvKEMtXgj9VjQ4TZ5IiYU7L5aFtpFr");
+  }
+}
